f54b1379fa695c1d7d7d28229afe00b03fcdf8c3
nihatkhidirov <nihatmkh@code.edu.az>
2024-02-13 01:38:22 +0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿#region Task1
+//int num =21;
+//if (num == 0)
+//{
+//    Console.WriteLine("Eded Duzgun Daxil Edilmemisdir");
+//}
+//else
+//{
+//    if (num%2==0)
+//    {
+//        Console.WriteLine("Eded Cutdur");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Eded tekdir");
+
+//    }
+//}
+#endregion
+#region Task2
+int num2 = 42;
+if (num2 % 21 == 0)
+{
+    Console.WriteLine("Eded 3-e ve 7-e bolunur");
+}
+else
+{
+    Console.WriteLine("Eded 3-e ve 7-e bolunmur");
+}
+#endregion 
+#region Task3
+int num3 = 100; int num4 = 202;
+if(num3%2==0 && num4 % 2 == 0)
+{
+    int newnum = num3 + num4;
+    Console.WriteLine(newnum);
+}
+else
+{
+    Console.WriteLine("Ededlerin her ikisi cut deyil");
+};
+#endregion
+#region Task4
+int temperature = 10;
+if (temperature < 15)
+{
+    Console.WriteLine("Hava Soyuqdur");
+}
+else
+{
+    Console.WriteLine("Hava Istidir");
+}
+
+#endregion
